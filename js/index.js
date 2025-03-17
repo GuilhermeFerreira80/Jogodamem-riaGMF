5b6965e6d9dc935ca36a1752a8b870e23cf0d854
GuilhermeFerreira80 <149723734+GuilhermeFerreira80@users.noreply.github.com>
2025-03-17 10:26:52 +0000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ const btLevel = document.querySelector("#btLevel");
 const btPlay = document.querySelector("#btPlay");
 const message = document.querySelector("#message");
 const infoGame = panelControl.querySelectorAll(".list-item");
-let flippedCards = [];
-let totalFlippedCards = 0;
+const labelGameTime = document.querySelector("#gameTime");
+const labelPoints = document.querySelector("#points");
 let cards = document.querySelectorAll(".card");
 let cardsLogos = [
   "angular",
@@ -21,7 +21,13 @@ let cardsLogos = [
   "ember",
 ];
 
-// Algoritmo Fisher-Yates -  Algoritmo que baralha um array.
+let [flippedCards, totalFlippedCards, totalPoints] = [[], 0, 0];
+
+const TIMEOUTGAME = 60;
+let timer, timerId;
+
+const gameOver = () => totalFlippedCards === cards.length;
+
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -30,48 +36,84 @@ const shuffleArray = (array) => {
     array[j] = temp;
   }
 };
-
-function reset() {
-  btLevel.value = 1;
-  btLevel.disabled = true;
-  cards.forEach((card) => {
-    card.classList.remove("flipped", "inactive");     
-    card.querySelector(".card-front").classList.remove("grayscale");
-    card.removeEventListener("click", flipCard);
-  });
-  panelControl.querySelectorAll(".list-item").forEach((elemento) => {
-    elemento.classList.remove("gameStarted");
-  });
+function updatePoints(operation = "+") {
+  let n = (cards.length - totalFlippedCards) / 2;
+  n = n == 0 ? 1 : n;
+  totalPoints =
+    operation === "+"
+      ? totalPoints + timer * n
+      : totalPoints < 5
+      ? 0
+      : totalPoints - 5;
+  labelPoints.textContent = totalPoints;
+}
+function updateGameTime() {
+  timer--;
+  labelGameTime.textContent = `${timer}s`;
+  if (timer === 0) stopGame();
 }
 function flipCard() {
-  if (!this.classList.contains("flipped")) {
-    this.classList.add("flipped");
-    flippedCards.push(this);
-    if (flippedCards.length === 2) checkPair();
-  }
+  this.classList.add("flipped");
+  flippedCards.push(this);
+  if (flippedCards.length === 2) checkPair();
 }
-const gameOver = () => totalFlippedCards === cards.length;
-
 function checkPair() {
   let [card1, card2] = flippedCards;
-  const isMatch = card1.dataset.logo === card2.dataset.logo;
-  if (isMatch) {
+  if (card1.dataset.logo === card2.dataset.logo) {
     setTimeout(() => {
       card1.classList.add("inactive");
       card2.classList.add("inactive");
       card1.querySelector(".card-front").classList.add("grayscale");
       card2.querySelector(".card-front").classList.add("grayscale");
       totalFlippedCards += 2;
+      updatePoints("+");
       if (gameOver()) stopGame();
     }, 500);
   } else {
     setTimeout(() => {
       card1.classList.remove("flipped");
       card2.classList.remove("flipped");
+      card1.addEventListener("click", flipCard, { once: true });
+      card2.addEventListener("click", flipCard, { once: true });
+      updatePoints("-");
     }, 500);
   }
   flippedCards = [];
 }
+
+function createPanelGame() {
+  let nCards = [6, 12, 20];
+  let classPanel = ["intermedio", "avancado"];
+  nCards = nCards[btLevel.value - 1];
+  panelGame.className = classPanel[btLevel.value - 2] || "";
+  let templateCard = document.createElement("div");
+  templateCard.innerHTML =
+    "<div class='card'><img src='images/ls.png' class='card-back'/><img class='card-front'/></div>";
+  panelGame.innerHTML = "";
+  templateCard = templateCard.firstChild;
+  for (let i = 0; i < nCards; i++)
+    panelGame.appendChild(templateCard.cloneNode(true));
+  cards = panelGame.children;
+}
+
+let showGameInfo = (visible) =>
+  visible
+    ? infoGame.forEach((e) => e.classList.add("gameStarted"))
+    : infoGame.forEach((e) => e.classList.remove("gameStarted"));
+
+function reset() {
+  panelGame.style.display = message.classList.remove("hide");
+
+  [btPlay.disabled, message.textContent, panelGame.style.display] = [
+    btLevel.value === "0" ? true : false,
+    "",
+    btLevel.value === "0" ? "none" : "grid",
+  ];
+  showGameInfo(false);
+  createPanelGame();
+  [labelGameTime.textContent, labelPoints.textContent] = [`${TIMEOUTGAME}s`, 0];
+}
+
 const createAndShuffleCards = (array) => {
   shuffleArray(array);
   array.splice(cards.length / 2, Number.MAX_VALUE);
@@ -80,38 +122,42 @@ const createAndShuffleCards = (array) => {
 };
 
 function startGame() {
+  message.classList.add("hide");
+  btLevel.disabled = true;
   btPlay.textContent = "Terminar Jogo";
+  showGameInfo(true);
   let [indice, newCardLogos] = [0, [...cardsLogos]];
   createAndShuffleCards(newCardLogos);
-  panelControl
-    .querySelectorAll(".list-item")
-    .forEach((elemento) => elemento.classList.add("gameStarted"));
+
   for (let card of cards) {
     let cardFront = card.querySelector(".card-front");
     cardFront.src = `images/${newCardLogos[indice]}.png`;
     card.dataset.logo = newCardLogos[indice++];
-    card.addEventListener("click", flipCard);
+    card.addEventListener("click", flipCard, { once: true });
   }
-  flippedCards = [];
-  totalFlippedCards = 0;
-
-  
+  [flippedCards, totalFlippedCards, totalPoints] = [[], 0, 0];
+  [timer, timerId] = [TIMEOUTGAME, setInterval(updateGameTime, 1000)];
+  [labelGameTime.textContent, labelPoints.textContent] = [`${timer}s`, 0];
 }
-//---
-
 function stopGame() {
-  btPlay.textContent = "Iniciar Jogo";
-  message.classList.remove("hide");
-  modalGameOver.showModal();  
+  [
+    btLevel.disabled,
+    btPlay.textContent,
+    document.querySelector("#messageGameOver").textContent,
+  ] = [false, "Iniciar Jogo", `Pontuação:${totalPoints}`];
+  clearInterval(timerId);
+  modalGameOver.showModal();
+  modalGameOver.querySelector("#nickname").style = "display: none;";
+  document.querySelector("#messageGameOver").textContent =
+    "Pontuação: " + totalPoints;
 }
 
 btPlay.addEventListener("click", () =>
   btPlay.textContent === "Terminar Jogo" ? stopGame() : startGame()
 );
-panelGame.addEventListener(
-  "click",
-  () =>
-    (message.textContent =
-      message.textContent === "" ? "Clique em Iniciar o Jogo!" : "")
-);
+panelGame.addEventListener("click", () => {
+  message.textContent = message.textContent ? "" : "Clique em Iniciar o Jogo!";
+});
+btLevel.addEventListener("change", reset);
+
 reset();
